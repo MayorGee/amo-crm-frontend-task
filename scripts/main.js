72feb01f6fbe49921b35b7ee7862c4fcf491c52d
mayorCoder <adebayomayowa78@gmail.com>
2025-04-14 11:01:35 +0300

    moved styling to a separate file

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ import AmoCrmApiClient from './AmoCrmApiClient.js';
 import AmoCrmDealsApp from './AmoCrmDealsApp.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const accessToken = 'ACCESS_TOKEN';
+    const accessToken = 'ACCESS_TOKEN'; // 
     const apiClient = new AmoCrmApiClient(accessToken);
     const app = new AmoCrmDealsApp(apiClient);
     
